3236990a36f1969948942d26a36918cc249fb168
Reed Salus <salusreed@microsoft.com>
2024-08-05 16:31:08 -0700

    apphost refs

--- src/Clutch.API.AppHost/Program.cs
+++ src/Clutch.API.AppHost/Program.cs
@@ -1,5 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Clutch_API>("clutch-api");
+// Add our redis cache and postgres DB to management
+// One question I still have is how this is converted to remote deployments
+// IE are these resources pushed to the managed services on Azure?
+var redis = builder.AddRedis("redis");
+var postgres = builder.AddPostgres("postgres");
+// We can specify the exact image to use:
+// .WithImage
+// .WithImageTag
+
+var containerImageDb = postgres.AddDatabase("containerImageDb");
+
+builder.AddProject<Projects.Clutch_API>("clutch-api")
+    .WithReference(redis)
+    .WithReference(containerImageDb);
 
 builder.Build().Run();
