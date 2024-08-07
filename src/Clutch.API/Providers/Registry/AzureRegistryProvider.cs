f080ac387ec183ea4fb1a09a6a71491ca286dfdd
Reed Salus <salusreed@microsoft.com>
2024-08-06 18:10:43 -0700

    azure auth

--- src/Clutch.API/Providers/Registry/AzureRegistryProvider.cs
+++ src/Clutch.API/Providers/Registry/AzureRegistryProvider.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json;
 using RestSharp;
 using Azure.Provisioning.ResourceManager;
 using Azure.Core;
+using Amazon.Runtime.Internal;
+using Google.Apis.Auth.OAuth2;
 
 namespace Clutch.API.Providers.Registry
 {
@@ -16,11 +18,16 @@ namespace Clutch.API.Providers.Registry
         private readonly ILogger _logger = logger;
         private readonly IConfiguration _configuration = configuration;
         private IRestClientFactory _restClientFactory = restClientFactory;
-        private readonly string azureToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(configuration.GetConnectionString("AzureToken") ?? string.Empty));
+        // Azure auth
+        private readonly string tenantId = Convert.ToBase64String(Encoding.UTF8.GetBytes(configuration["Azure:AzureTenantId"] ?? string.Empty));
+        private readonly string azureClientId = Convert.ToBase64String(Encoding.UTF8.GetBytes(configuration["Azure:AzureClientId"] ?? string.Empty));
+        private readonly string azureClientSecret = Convert.ToBase64String(Encoding.UTF8.GetBytes(configuration["Secrets:AzureClientSecret"] ?? string.Empty));
+        private readonly string azureScope = Convert.ToBase64String(Encoding.UTF8.GetBytes(configuration["Azure:AzureScope"] ?? string.Empty));
 
         public override async Task<RegistryManifestModel> GetManifestAsync(ContainerImageRequest request)
         {
             string[] parts = request.Repository.Split('/');
+            dynamic azureToken = GetToken(parts);
 
             // Construct the API request
             _restClientFactory.InstantiateClient("https://registry-1.docker.io");
@@ -33,6 +40,46 @@ namespace Clutch.API.Providers.Registry
             RestResponse? response = await _restClientFactory.ExecuteAsync(restRequest);
 
             return ValidateAndDeserializeResponse(response);
+        }
+
+        private async Task<dynamic> GetToken(string[] parts)
+        {
+            dynamic rawToken;
+
+            // Get the Access Token
+            _restClientFactory.InstantiateClient("https://login.microsoftonline.com");
+            RestRequest authRequest = new($"/{tenantId}/oauth2/v2.0/token");
+            authRequest.Method = Method.Post;
+            authRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            authRequest.AddParameter("grant_type", "client_credentials");
+            authRequest.AddParameter("client_id", azureClientId);
+            authRequest.AddParameter("client_secret", azureClientSecret);
+            authRequest.AddParameter("scope", azureScope);
+
+            RestResponse? authResponse = await _restClientFactory.ExecuteAsync(authRequest);
+            if (authResponse is null)
+            {
+                _logger.LogError("Failed to retrieve image manifest from registry due to an exception processing the request.");
+                return RegistryManifestModel.Null;
+            }
+            else if (authResponse.StatusCode != HttpStatusCode.OK || authResponse.Content is null)
+            {
+                _logger.LogError("Failed to retrieve bearer token for registry. StatusCode: {StatusCode}. ErrorMessage: {ErrorMessage}", authResponse.StatusCode, authResponse.ErrorMessage);
+                return RegistryManifestModel.Null;
+            }
+
+            try
+            {
+                rawToken = JsonConvert.DeserializeObject(authResponse.Content) ?? string.Empty;
+                if (rawToken is string) throw new DeserializationException(authResponse, new("Deserialization returned null."));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Failed to deserialize bearer token. Exception: {ex}", ex.GetBaseException());
+                return RegistryManifestModel.Null;
+            }
+
+            return rawToken;
         }
     }
 }
