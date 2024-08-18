955cb113434951bba3552caba3458460631be8bb
Reed Salus <salusreed@microsoft.com>
2024-08-18 01:36:31 -0700

    database

--- .github/actions/update-database/update-database.js
+++ .github/actions/update-database/update-database.js
@@ -6,56 +6,55 @@
 const { Client } = require('pg');
 const core = require('@actions/core'); 
 
-async function run() {
+async function main() {
   try {
-    // Get required inputs from the Action's YAML file
-    const connectionString = core.getInput('postgresql_connection_string', { required: true });
-    const tableToUpdate = core.getInput('table_to_update', { required: true });
-    const indexToUpdate = core.getInput('index_to_update', { required: true });
-    const operation = core.getInput('operation', { required: true });
-    const updateValue = core.getInput('update_value', { required: true });
-    
-    // Create a PostgreSQL client
-    const client = new Client({
-      connectionString: connectionString
-    });
+      // Get required inputs from the Action's YAML file
+      const connectionString = core.getInput('postgresql_connection_string', { required: true });
+      const tableToUpdate = core.getInput('table_to_update', { required: true });
+      const indexToUpdate = core.getInput('index_to_update', { required: true });
+      const operation = core.getInput('operation', { required: true });
+      const updateValue = core.getInput('update_value', { required: true });
+      
+      // Create a PostgreSQL client
+      const client = new Client({
+        connectionString: connectionString
+      });
 
-    // Connect to the database
-    await client.connect();
+      // Connect to the database
+      await client.connect();
 
-    // Construct and execute the SQL queries
-    const updateQuery = `
-        UPDATE ${tableToUpdate}
-        SET Status = $1
-        WHERE RepositoryId = $2; 
-    `;
+      // Construct and execute the SQL queries
+      const updateQuery = `
+          UPDATE ${tableToUpdate}
+          SET Status = $1
+          WHERE RepositoryId = $2; 
+      `;
 
-    const deleteQuery = `
-        DELETE FROM ${tableToUpdate}
-        WHERE RepositoryId = $2; 
-    `;
+      const deleteQuery = `
+          DELETE FROM ${tableToUpdate}
+          WHERE RepositoryId = $2; 
+      `;
 
-    const values = [updateValue, indexToUpdate];
-    let result = null;
+      const values = [updateValue, indexToUpdate];
+      let result = null;
 
-    // Execute operation
-    if (operation.toLowerCase() === 'update') {
-      result = await client.query(updateQuery, values);
-    } else if (operation.toLowerCase() === 'delete') {
-      result = await client.query(deleteQuery, values);
-    }
+      // Execute operation
+      if (operation.toLowerCase() === 'update') {
+        result = await client.query(updateQuery, values);
+      } else if (operation.toLowerCase() === 'delete') {
+        result = await client.query(deleteQuery, values);
+      }
 
-    // Check if any rows were affected
-    if (result == null || result.rowCount === 0) {
-      throw new Error(`No rows were edited in table ${tableToUpdate}. Check if the index exists.`);
-    }
+      // Check if any rows were affected
+      if (result == null || result.rowCount === 0) {
+        throw new Error(`No rows were edited in table ${tableToUpdate}. Check if the index exists.`);
+      }
 
-    core.info(`Successfull ${operation} operation for row ${indexToUpdate} in table ${tableToUpdate}`);
-    core.setOutput('result', true);
+      core.info(`Successfull ${operation} operation for row ${indexToUpdate} in table ${tableToUpdate}`);
+      core.setOutput('result', true);
 
-    // Release the connection
-    await client.end();
-
+      // Release the connection
+      await client.end();
   } catch (error) {
       core.error(error.message);
       core.setFailed(`Exiting with error: ${error}`);
@@ -64,4 +63,4 @@ async function run() {
   }
 }
 
-run();
+main();
