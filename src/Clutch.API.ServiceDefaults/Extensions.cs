1a126f54f65f41271a2664b648234f9e03df050b
Reed Salus <reedsalus@gmail.com>
2024-09-03 23:11:29 -0700

    updated servicedefaults

--- src/Clutch.API.ServiceDefaults/Extensions.cs
+++ src/Clutch.API.ServiceDefaults/Extensions.cs
@@ -32,6 +32,12 @@ public static class Extensions
             http.AddServiceDiscovery();
         });
 
+        // Uncomment the following to restrict the allowed schemes for service discovery.
+        // builder.Services.Configure<ServiceDiscoveryOptions>(options =>
+        // {
+        //     options.AllowedSchemes = ["https"];
+        // });
+
         return builder;
     }
 
